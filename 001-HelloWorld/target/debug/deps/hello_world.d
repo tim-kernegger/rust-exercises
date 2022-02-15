8e84cbf284c4721c3694840f4d7def87b28cf92d
tim-kernegger <kernegger.ti@gmail.com>
2022-02-15 22:40:21 +0100

    complete exercise 001

---
+++ 001-HelloWorld/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\timke\projects\rust-exercises\001-HelloWorld\target\debug\deps\hello_world.exe: src\main.rs
+
+C:\Users\timke\projects\rust-exercises\001-HelloWorld\target\debug\deps\hello_world.d: src\main.rs
+
+src\main.rs:
---
+++ 001-HelloWorld/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+C:\Users\timke\projects\rust-exercises\001-HelloWorld\target\debug\hello-world.exe: C:\Users\timke\projects\rust-exercises\001-HelloWorld\src\main.rs
